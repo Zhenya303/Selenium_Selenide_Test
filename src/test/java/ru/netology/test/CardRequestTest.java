c0035e4aaed3c59e1c65935e4e5691a73991b8e5
“Zhenya” <dyadicheve@mail.ru>
2023-07-09 17:36:08 +0300

    CardRequestTestUpdate

--- src/test/java/ru/netology/test/CardRequestTest.java
+++ src/test/java/ru/netology/test/CardRequestTest.java
@@ -1,85 +1,83 @@
 package ru.netology.test;
 
 
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardRequestTest {
 
+    @BeforeEach
+
+    public void setup(){
+        open("http://localhost:9999");
+
+    }
+
+
+
     @Test
 
     public void shouldSubmitRequest() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").sendKeys("Иванов Иван");
         $("[data-test-id=phone] input").sendKeys("+79999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! " +
+                "Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
 
     public void shouldAlertIfNameFieldEmpty() {
-        open("http://localhost:9999");
         $("[data-test-id=phone] input").sendKeys("+79999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        String expected = "Поле обязательно для заполнения";
-        String actual = $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
     }
 
     @Test
     public void shouldAlertIfWrongName() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").sendKeys("John");
         $("[data-test-id=phone] input").sendKeys("+79999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. " +
+                "Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
 
     public void shouldAlertIfPhoneNumberFieldIsEmpty() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").sendKeys("Иван Иванов");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        String expected = "Поле обязательно для заполнения";
-        String actual = $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
 
     }
 
     @Test
     public void shouldAlertIfWrongPhoneNumber() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").sendKeys("Иванов Иван");
         $("[data-test-id=phone] input").sendKeys("+79");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. " +
+                "Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
 
     public void shouldNotSubmitIfNotSignedAgreement() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").sendKeys("Иван Иванов");
         $("[data-test-id=phone] input").sendKeys("+79999999999");
         $("button").click();
-        Assertions.assertTrue($(By.cssSelector("[data-test-id=agreement].input_invalid")).isDisplayed());
+        $("[data-test-id=agreement].input_invalid").shouldBe(visible);
     }
 
 
